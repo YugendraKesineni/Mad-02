521197b9202389259f6a4142372241571c864bf7
YugendraKesineni <76766577+YugendraKesineni@users.noreply.github.com>
2023-07-04 13:06:51 +0000

    Hello

--- VueJS/Section-02/basics-03-events-starting-code/app.js
+++ VueJS/Section-02/basics-03-events-starting-code/app.js
@@ -2,8 +2,31 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name : "",
+      confirmedName: ""
     };
   },
+
+  methods: {
+
+    confirmedInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert("SUbmitted");
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+
+    setName(lastname) {
+      this.name = event.target.value +" "+ lastname;
+    }
+  }
 });
 
 app.mount('#events');
